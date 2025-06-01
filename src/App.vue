338be1e0a591c7cf3650f9073170ee6e987a8c5c
EmannCode01 <olajubajeifeoluwa93@gmail.com>
2025-06-01 14:42:11 +0100

    new

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
 import Herocard from "./components/Herocard.vue";
-import Brand from "./components/Brand.vue"
+import Brand from "./components/Brand.vue";
+import NewArrivals from "./components/NewArrivals.vue";
 </script>
 
 <template>
   <Navbar />
   <Herocard />
   <Brand />
+  <NewArrivals />
 </template>
 
 <style scoped></style>
---
+++ src/components/NewArrivals.vue
@@ -0,0 +1,29 @@
+<script setup>
+    import vector8 from "../assets/vector8.svg";
+</script>
+
+<template>
+    <div class="card">
+        <div class="header">
+            <h1>New Arrivals</h1>
+            <img :src="vector8" alt="Vector Image">
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    margin: 2em 3em;
+    letter-spacing: 1px;
+}
+
+.header {
+    display: flex;
+    align-items: center;
+    gap: 1em;
+}
+
+h1 {
+    font-weight: 700;
+}
+</style>
